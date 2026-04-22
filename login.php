8efbcd4553ecc85a91a1ab1de52a7d4edf8a0cc7
Ayah Alyasiri <ayah200324@gmail.com>
2026-04-22 09:59:08 +0300

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,15 @@
  */
 require_once 'config.php';
 
+
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
+
 // --- نظام الترجمة الاحترافي (بدون جوجل) ---
 if (!isset($_SESSION['lang'])) { $_SESSION['lang'] = 'en'; }
 if (isset($_GET['tplang'])) {
